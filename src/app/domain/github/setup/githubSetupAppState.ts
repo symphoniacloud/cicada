53fd6f1a7ceafe54ec6d0af7147177a7533c9c32
Mike Roberts <mike@symphonia.io>
2024-04-19 19:35:24 -0400

    Allow user to enter organization name during setup

--- src/app/domain/github/setup/githubSetupAppState.ts
+++ src/app/domain/github/setup/githubSetupAppState.ts
@@ -5,7 +5,6 @@ import { paramsForAppName } from '../../../environment/config'
 export interface GithubSetupAppState {
   appName: string
   githubAppId?: string
-  allowedAccountName: string
   webHostname: string
   webhookCode: string
   callbackState: string
@@ -17,7 +16,6 @@ export async function githubSetupStartup(): Promise<GithubSetupAppState> {
   return {
     appName,
     githubAppId: await params.getParamOrUndefined(SSM_PARAM_NAMES.GITHUB_APP_ID, { maxAge: 5 }),
-    allowedAccountName: await params.getParam(SSM_PARAM_NAMES.CONFIG_ALLOWED_INSTALLATION_ACCOUNT_NAME),
     webHostname: await params.getParam(SSM_PARAM_NAMES.WEB_HOSTNAME),
     webhookCode: await params.getParam(SSM_PARAM_NAMES.GITHUB_WEBHOOK_URL_CODE),
     callbackState: await params.getParam(SSM_PARAM_NAMES.GITHUB_CALLBACK_STATE)
