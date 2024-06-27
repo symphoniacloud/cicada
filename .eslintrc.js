52dc0db26545713c765c7041c4ad4cb6f822aaa9
Mike Roberts <mike@symphonia.io>
2024-06-27 16:43:31 -0400

    Use self hosted version of HTMX and bootstrap
    * HTMX from https://unpkg.com/htmx.org/dist/htmx.min.js
    * Bootstrap from https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    env: {
-        node: true,
-        es2022: true
-    },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    overrides: [],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-    },
-    plugins: ['@typescript-eslint']
+  env: {
+    node: true,
+    es2022: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  ignorePatterns: ['htmx.min.js'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint']
 }
