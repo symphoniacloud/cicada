351d1fd55614be436300b74dd66121133bf1b10a
Mike Roberts <mike@symphonia.io>
2024-09-04 15:17:55 -0400

    Use user config for home screen
    Don't show workflows on home screen that aren't shown via user settings

--- src/app/domain/user/userVisible.ts
+++ src/app/domain/user/userVisible.ts
@@ -3,19 +3,50 @@ import { getAllAccountIdsForUser } from '../github/githubMembership'
 import { latestWorkflowRunEventsPerWorkflowForOwners } from '../github/githubLatestWorkflowRunEvents'
 import { GithubWorkflow } from '../types/GithubWorkflow'
 import { GithubWorkflowRunEvent } from '../types/GithubWorkflowRunEvent'
+import { getUserSettings } from './persistedUserSettings'
+import { calculateUserSettings } from './calculatedUserSettings'
+import { CalculatedUserSettings } from '../types/UserSettings'
 
-export async function getLatestWorkflowRunEventsForUser(
+export async function getLatestVisibleWorkflowRunEventsForUser(
   appState: AppState,
   userId: number
-): Promise<GithubWorkflowRunEvent[]> {
-  // Workflow run events extend workflow, so we can just return the latest run events
-  return await latestWorkflowRunEventsPerWorkflowForOwners(
-    appState,
-    await getAllAccountIdsForUser(appState, userId)
-  )
+): Promise<{
+  allEvents: GithubWorkflowRunEvent[]
+  visibleEvents: GithubWorkflowRunEvent[]
+  someWorkflowsInvisible: boolean
+}> {
+  const allEvents = await getAllLatestRunEventsForUser(appState, userId)
+  const userSettings = calculateUserSettings(await getUserSettings(appState, userId), allEvents)
+  const filteredEvents = filterOutConfiguredInvisibleWorkflowEvents(allEvents, userSettings)
+  return {
+    allEvents,
+    visibleEvents: filteredEvents,
+    someWorkflowsInvisible: allEvents.length !== filteredEvents.length
+  }
 }
 
 export async function getWorkflowsForUser(appState: AppState, userId: number): Promise<GithubWorkflow[]> {
   // Workflow run events extend workflow, so we can just return the latest run events
-  return getLatestWorkflowRunEventsForUser(appState, userId)
+  return getAllLatestRunEventsForUser(appState, userId)
+}
+
+export function filterOutConfiguredInvisibleWorkflowEvents(
+  allEvents: GithubWorkflowRunEvent[],
+  userSettings: CalculatedUserSettings
+) {
+  return allEvents.filter(
+    ({ ownerId, repoId, workflowId }) =>
+      userSettings.github.accounts.get(ownerId)?.repos.get(repoId)?.workflows.get(workflowId)?.visible ??
+      false
+  )
+}
+
+async function getAllLatestRunEventsForUser(
+  appState: AppState,
+  userId: number
+): Promise<GithubWorkflowRunEvent[]> {
+  return await latestWorkflowRunEventsPerWorkflowForOwners(
+    appState,
+    await getAllAccountIdsForUser(appState, userId)
+  )
 }
