84498bb988634bc3714f41933199a7b8dbdfb3bf
Mike Roberts <mike@symphonia.io>
2024-10-04 15:56:41 -0400

    Refactoring

--- src/app/web/routingCommon.ts
+++ src/app/web/routingCommon.ts
@@ -6,6 +6,10 @@ export function fragmentPath(suffix: string) {
   return `/app/fragment/${suffix}`
 }
 
+export function isFragmentPath(path: string) {
+  return path.startsWith('/app/fragment/')
+}
+
 export function authenticateApiPath(suffix: string) {
   return `/apia/${suffix}`
 }
