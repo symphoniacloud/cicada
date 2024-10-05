7ec4baf7fee4ca429f663352b372c5bc869c7c2d
Mike Roberts <mike@symphonia.io>
2024-10-05 18:18:05 -0400

    Prioritize member account on settings screen

--- test/manual/workbench.ts
+++ test/manual/workbench.ts
@@ -1,17 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { appStateForTests } from '../remote/integrationTestSupport/cloudEnvironment'
-
-import { fromRawGithubInstallationId } from '../../src/app/domain/types/GithubInstallationId'
+import { loadCalculatedAndDisplayableUserSettingsOrUseDefaults } from '../../src/app/domain/user/displayableUserSettings'
+import { loadUserScopeReferenceData } from '../../src/app/domain/github/userScopeReferenceData'
 
 async function run() {
   const appState = await appStateForTests()
-  const githubClient = appState.githubClient.clientForInstallation(fromRawGithubInstallationId(50081716))
-  const result = await githubClient.getUser('mikebroberts')
+  const result = await loadCalculatedAndDisplayableUserSettingsOrUseDefaults(
+    appState,
+    await loadUserScopeReferenceData(appState, 'GHUser49635')
+  )
   console.log(result)
-  // await crawlGithubApp(appState, { crawlChildObjects: 'always', lookbackDays: 180 })
 }
 
 run()
-
-type ZooUser = `GU-${number}`
