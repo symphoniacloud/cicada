c0cc0624dd47e4371e0a4f3152d680235bf41b8d
Mike Roberts <mike@symphonia.io>
2024-10-07 10:54:56 -0400

    Logging updates

--- src/app/domain/github/githubPush.ts
+++ src/app/domain/github/githubPush.ts
@@ -8,7 +8,15 @@ import { sendToEventBridge } from '../../outboundInterfaces/eventBridgeBus'
 import { saveLatestPushes } from './githubLatestPushesPerRef'
 
 export async function processPushes(appState: AppState, pushes: GithubPush[], publishNotifications: boolean) {
+  if (pushes.length > 0) {
+    logger.debug(`Processing ${pushes.length} pushes for ${pushes[0].accountName}/${pushes[0].repoName}`)
+  }
   const newPushes = await savePushes(appState, pushes)
+  if (newPushes.length > 0) {
+    logger.info(
+      `Found ${newPushes.length} new pushes for ${newPushes[0].accountName}/${newPushes[0].repoName}`
+    )
+  }
   await saveLatestPushes(appState, newPushes)
   for (const newEvent of publishNotifications ? newPushes : []) {
     await sendToEventBridge(appState, EVENTBRIDGE_DETAIL_TYPES.GITHUB_NEW_PUSH, newEvent)
@@ -16,7 +24,7 @@ export async function processPushes(appState: AppState, pushes: GithubPush[], pu
 }
 
 async function savePushes(appState: AppState, pushes: GithubPush[]) {
-  const newPushes = (
+  return (
     await Promise.all(
       pushes.map(async (push) => {
         return executeAndCatchConditionalCheckFailed(async () => {
@@ -25,9 +33,6 @@ async function savePushes(appState: AppState, pushes: GithubPush[]) {
       })
     )
   ).filter((x): x is GithubPush => x !== undefined)
-  logger.debug(`Found ${newPushes.length} new pushes`)
-
-  return newPushes
 }
 
 export function latestCommitInPush(push: Pick<GithubPush, 'commits'>) {
