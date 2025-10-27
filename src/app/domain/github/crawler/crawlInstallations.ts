5febb06d6a3d30e183b55ea9b93b3b26c8570234
Mike Roberts <mike@symphonia.io>
2025-10-27 18:17:11 -0400

    Start on zod updates for crawlers

--- src/app/domain/github/crawler/crawlInstallations.ts
+++ src/app/domain/github/crawler/crawlInstallations.ts
@@ -3,10 +3,12 @@ import { AppState } from '../../../environment/AppState.js'
 import { removeNullAndUndefined } from '../../../util/collections.js'
 import { logger } from '../../../util/logging.js'
 import { GitHubInstallation } from '../../../ioTypes/GitHubTypes.js'
+import { RawGithubInstallationSchema } from '../../types/rawGithub/RawGithubInstallation.js'
 
 export async function crawlInstallations(appState: AppState): Promise<GitHubInstallation[]> {
   logger.info(`Crawling Installations`)
-  const installations = await appState.githubClient.listInstallations()
+  const raw = await appState.githubClient.listInstallations()
+  const installations = raw.map((rawI) => RawGithubInstallationSchema.parse(rawI))
 
   return removeNullAndUndefined(
     await Promise.all(installations.map(async (raw) => processRawInstallation(appState, raw)))
