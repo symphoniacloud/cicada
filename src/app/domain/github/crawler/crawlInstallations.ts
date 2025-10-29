01b50bc55ff6c052fcc95565b00cec677f419c0b
Mike Roberts <mike@symphonia.io>
2025-10-29 17:30:32 -0400

    More zod updates
    Starting use of explicit transform

--- src/app/domain/github/crawler/crawlInstallations.ts
+++ src/app/domain/github/crawler/crawlInstallations.ts
@@ -1,16 +1,19 @@
-import { processRawInstallation } from '../githubInstallation.js'
+import { processInstallation } from '../githubInstallation.js'
 import { AppState } from '../../../environment/AppState.js'
 import { removeNullAndUndefined } from '../../../util/collections.js'
 import { logger } from '../../../util/logging.js'
 import { GitHubInstallation } from '../../../ioTypes/GitHubTypes.js'
-import { RawGithubInstallationSchema } from '../../types/rawGithub/RawGithubInstallation.js'
+import { TransformedGithubInstallationSchema } from '../../types/rawGithub/RawGithubInstallation.js'
 
 export async function crawlInstallations(appState: AppState): Promise<GitHubInstallation[]> {
   logger.info(`Crawling Installations`)
-  const raw = await appState.githubClient.listInstallations()
-  const installations = raw.map((rawI) => RawGithubInstallationSchema.parse(rawI))
 
+  const installationsFromGitHub = await appState.githubClient.listInstallations()
   return removeNullAndUndefined(
-    await Promise.all(installations.map(async (raw) => processRawInstallation(appState, raw)))
+    await Promise.all(
+      installationsFromGitHub
+        .map((rawI) => TransformedGithubInstallationSchema.parse(rawI))
+        .map(async (installation) => processInstallation(appState, installation))
+    )
   )
 }
