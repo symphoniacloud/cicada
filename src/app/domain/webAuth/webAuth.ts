abc9f43991c491d3cba53a04ddf8985f8b726e46
Mike Roberts <mike@symphonia.io>
2025-10-31 16:50:56 -0400

    More zod updates

--- src/app/domain/webAuth/webAuth.ts
+++ src/app/domain/webAuth/webAuth.ts
@@ -1,20 +1,11 @@
 // If token valid then username will be available here
 
 import { CicadaAPIAuthorizedAPIEvent } from '../../inboundInterfaces/lambdaTypes.js'
-import { logger } from '../../util/logging.js'
-import { isGitHubUserId } from '../../ioTypes/GitHubTypeChecks.js'
 import { GitHubUserId } from '../../ioTypes/GitHubTypes.js'
+import { GitHubUserIdSchema } from '../../ioTypes/GitHubSchemas.js'
 
 export function userIdFromEvent(event: CicadaAPIAuthorizedAPIEvent): GitHubUserId {
-  const userId = event.requestContext.authorizer.userId
-
-  if (!isGitHubUserId(userId)) {
-    const message = `Invalid Github User ID on API Event: ${userId}`
-    logger.error(message)
-    throw new Error(message)
-  }
-
-  return userId
+  return GitHubUserIdSchema.parse(event.requestContext.authorizer.userId)
 }
 
 export function usernameFromEvent(event: CicadaAPIAuthorizedAPIEvent): string | undefined {
