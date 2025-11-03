df13977085c946e92e5ed291f7a76b3dc9d77781
Mike Roberts <mike@symphonia.io>
2025-11-03 14:43:51 -0500

    More zod

--- src/app/domain/webAuth/webAuth.ts
+++ src/app/domain/webAuth/webAuth.ts
@@ -1,6 +1,7 @@
 import { CicadaAPIAuthorizedAPIEvent } from '../../inboundInterfaces/lambdaTypes.js'
-import { GitHubUserId } from '../../ioTypes/GitHubTypes.js'
+import { GitHubUserId, GitHubUserSummary } from '../../ioTypes/GitHubTypes.js'
 import { GitHubUserIdSchema } from '../../ioTypes/GitHubSchemas.js'
+import { APIEvent } from '../../ioTypes/zodUtil.js'
 
 export function userIdFromEvent(event: CicadaAPIAuthorizedAPIEvent): GitHubUserId {
   return GitHubUserIdSchema.parse(event.requestContext.authorizer.userId)
@@ -8,4 +9,15 @@ export function userIdFromEvent(event: CicadaAPIAuthorizedAPIEvent): GitHubUserI
 
 export function usernameFromEvent(event: CicadaAPIAuthorizedAPIEvent): string | undefined {
   return event.requestContext.authorizer.username
+}
+
+export function userIdFromApiEvent(event: APIEvent): GitHubUserId {
+  return event.requestContext.authorizer.userId
+}
+
+export function githubUserSummaryFromEvent(event: APIEvent): GitHubUserSummary {
+  return {
+    userId: userIdFromApiEvent(event),
+    userName: event.requestContext.authorizer.username
+  }
 }
