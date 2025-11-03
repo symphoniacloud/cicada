71dc3dd3e3f5717782498dca1dcfbb6e519abe56
Mike Roberts <mike@symphonia.io>
2025-11-03 11:43:32 -0500

    More zod refactoring
    Safely handle ignored webhooks

--- src/app/domain/github/webhookProcessor/processors/githubWebhookWorkflowRunProcessor.ts
+++ src/app/domain/github/webhookProcessor/processors/githubWebhookWorkflowRunProcessor.ts
@@ -12,24 +12,17 @@ export const githubWebhookWorkflowRunProcessor: WebhookProcessor = async (
   appState: AppState,
   body: string
 ): Promise<void> => {
-  const parseResult = GitHubWebhookWorkflowRunEventSchema.safeParse(body)
-
-  if (!parseResult.success) {
-    logger.warn('Unexpected workflow_run format - not processing')
-    return
-  }
-
-  const parsed = parseResult.data.workflow_run
+  const rawWorkflowRun = GitHubWebhookWorkflowRunEventSchema.parse(body).workflow_run
 
   const installation = await getInstallationOrUndefined(
     appState.entityStore,
-    fromRawGitHubAccountId(parsed.repository.owner.id)
+    fromRawGitHubAccountId(rawWorkflowRun.repository.owner.id)
   )
   if (!installation) {
-    logger.warn('Received Run event but no known installation for run event owner', { parsed })
+    logger.warn('Received Run event but no known installation for run event owner', { rawWorkflowRun })
     return
   }
-  const githubClient = appState.githubClient.clientForInstallation(installation.installationId)
 
-  await processRawRunEvent(appState, parsed, githubClient, true)
+  const githubClient = appState.githubClient.clientForInstallation(installation.installationId)
+  await processRawRunEvent(appState, rawWorkflowRun, githubClient, true)
 }
