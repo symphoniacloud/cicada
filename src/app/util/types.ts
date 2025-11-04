ad6c2333344ce148cda9c479dd2cfc1622f14c2e
Mike Roberts <mike@symphonia.io>
2025-11-04 09:15:56 -0500

    Cleanup

--- src/app/util/types.ts
+++ src/app/util/types.ts
@@ -1,33 +1,5 @@
-export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
 export type Mandatory<T, K extends keyof T> = Pick<Required<T>, K> & Omit<T, K>
-
-export function isNumber(x: unknown): x is number {
-  return typeof x === 'number'
-}
-
-export function isString(x: unknown): x is string {
-  return typeof x === 'string'
-}
-
-export function isStringOrUndefined(x: unknown): x is string {
-  return x === undefined || typeof x === 'string'
-}
 
 export function isNotNullObject(x: unknown): x is NonNullable<object> {
   return x !== null && typeof x === 'object'
-}
-
-export function stringIsUnpaddedNotEmptyNonNegativeInteger(x: string): x is `${number}` {
-  if (x.length === 0 || x.trim().length !== x.length) return false
-  const xAsNumber = Number(x)
-  return Number.isInteger(xAsNumber) && xAsNumber >= 0
-}
-
-export function isIntegerStringWithPrefix<TPrefix extends string>(
-  prefix: TPrefix,
-  x: unknown
-): x is `${TPrefix}${number}` {
-  return (
-    isString(x) && x.startsWith(prefix) && stringIsUnpaddedNotEmptyNonNegativeInteger(x.slice(prefix.length))
-  )
 }
--- src/multipleContexts/githubCrawler.ts
+++ src/multipleContexts/githubCrawler.ts
@@ -4,9 +4,3 @@ export const CRAWLABLE_RESOURCES = {
   PUBLIC_ACCOUNTS: 'publicAccounts',
   PUBLIC_ACCOUNT: 'publicAccount'
 } as const
-
-export type CrawlableResource = (typeof CRAWLABLE_RESOURCES)[keyof typeof CRAWLABLE_RESOURCES]
-
-export function isCrawlableResource(x: unknown): x is CrawlableResource {
-  return typeof x === 'string' && Object.values(CRAWLABLE_RESOURCES).includes(x as CrawlableResource)
-}
