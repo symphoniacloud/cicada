e6dd7b90d5255354d6cb38f2b38e8b8cfa330bca
Mike Roberts <mike@symphonia.io>
2025-11-04 14:52:40 -0500

    cleanup

--- src/app/web/htmlRequests.ts
+++ src/app/web/htmlRequests.ts
@@ -3,12 +3,24 @@ import { z } from 'zod'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { Result } from '../util/structuredResult.js'
 import { invalidRequestResponse } from './htmlResponses.js'
-import { parseAPIGatewayEventQueryStringWithSchema } from '../inboundInterfaces/httpRequests.js'
+import {
+  parseAPIGatewayEventQueryStringWithSchema,
+  parsePartialAPIGatewayEventQueryStringWithSchema
+} from '../inboundInterfaces/httpRequests.js'
+import { CicadaAuthorizedAPIEvent } from '../inboundInterfaces/lambdaTypes.js'
 
-export function parseQueryStringWithSchema<T extends z.ZodTypeAny>(
+export function parseQueryStringWithSchema<T extends z.ZodType>(
   schema: T,
   event: APIGatewayProxyEvent,
   loggingDetail?: string
 ): Result<z.infer<T>, APIGatewayProxyResult> {
   return parseAPIGatewayEventQueryStringWithSchema(schema, event, invalidRequestResponse, loggingDetail)
+}
+
+export function parsePartialQueryStringWithSchema<T extends z.ZodObject<z.ZodRawShape>>(
+  schema: T,
+  event: CicadaAuthorizedAPIEvent,
+  errorContext: string
+): Result<Partial<z.infer<T>>, APIGatewayProxyResult> {
+  return parsePartialAPIGatewayEventQueryStringWithSchema(schema, event, invalidRequestResponse, errorContext)
 }
