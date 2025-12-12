9b592a9135eaf27fb927ae792386b938e73d07a9
Mike Roberts <mike@symphonia.io>
2025-12-12 17:32:38 -0500

    Update Vitest to 4.x
    Was waiting for Webstorm support

--- test/local/vitest.config.mjs
+++ test/local/vitest.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['**/*.test.js', '**/*.test.ts'],
-    exclude: ['node_modules/**']
+    include: ['**/*.test.js', '**/*.test.ts']
   }
 })
